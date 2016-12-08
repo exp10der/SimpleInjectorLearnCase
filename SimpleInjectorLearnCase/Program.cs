1f2d068a25634c477737c588bea7aa43e2ce7a0b
Konstantin <konstantin.writing.code@gmail.com>
2016-12-08 22:57:25 +0300

    (new) Create container

--- SimpleInjectorLearnCase/Program.cs
+++ SimpleInjectorLearnCase/Program.cs
@@ -1,11 +1,21 @@
 ﻿namespace SimpleInjectorLearnCase
 {
     using System;
+    using SimpleInjector;
 
     internal class Program
     {
         private static void Main()
         {
+            var container = new Container();
+            BuildContainer(container);
+
+        }
+
+        private static void BuildContainer(Container container)
+        {
+            container.Register<ISayHello, SayHello>(Lifestyle.Singleton);
+            container.Verify();
         }
     }
 

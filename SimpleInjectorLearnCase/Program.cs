d7c8f2a3eba1adc87ea03014601e9d507f5365b9
Konstantin <konstantin.writing.code@gmail.com>
2016-12-08 23:13:58 +0300

    (new) Add register ISayHelloWithCtor -> SayHelloWithCtor with ctor

--- SimpleInjectorLearnCase/Program.cs
+++ SimpleInjectorLearnCase/Program.cs
@@ -14,11 +14,17 @@
             var hello2 = container.GetInstance<ISayHello>();
 
             Console.WriteLine(ReferenceEquals(hello1, hello2));
+
+            var helloCtor1 = container.GetInstance<ISayHelloWithCtor>();
+            var helloCtor2 = container.GetInstance<ISayHelloWithCtor>();
+
+            Console.WriteLine(ReferenceEquals(helloCtor1, helloCtor2));
         }
 
         private static void BuildContainer(Container container)
         {
             container.Register<ISayHello, SayHello>(Lifestyle.Singleton);
+            container.Register<ISayHelloWithCtor>(() => new SayHelloWithCtor("ctor"), Lifestyle.Transient);
             container.Verify();
         }
     }
@@ -33,6 +39,26 @@
         public void Hello()
         {
             Console.WriteLine("Hello");
+        }
+    }
+
+    public interface ISayHelloWithCtor
+    {
+        void Hello();
+    }
+
+    public class SayHelloWithCtor : ISayHelloWithCtor
+    {
+        private readonly string _message;
+
+        public SayHelloWithCtor(string message)
+        {
+            _message = message;
+        }
+
+        public void Hello()
+        {
+            Console.WriteLine(_message);
         }
     }
 }

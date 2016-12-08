54e38520dec10b8a115e8936ddd21a3eaba5b54f
Konstantin <konstantin.writing.code@gmail.com>
2016-12-08 22:54:32 +0300

    (new) Create SayHello

--- SimpleInjectorLearnCase/Program.cs
+++ SimpleInjectorLearnCase/Program.cs
@@ -1,9 +1,24 @@
 ﻿namespace SimpleInjectorLearnCase
 {
+    using System;
+
     internal class Program
     {
         private static void Main()
         {
+        }
+    }
+
+    public interface ISayHello
+    {
+        void Hello();
+    }
+
+    public class SayHello : ISayHello
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Hello");
         }
     }
 }
